1cff75a2d316c9c2a2f3d2a5d6604403078abeda
Harel Lustiger <harel@loostiger.com>
2015-10-18 12:03:08 +0300

    upgraded labelingCostQualityTradeoff()

--- functions/labelingCostQualityTradeoff.R
+++ functions/labelingCostQualityTradeoff.R
@@ -1,41 +1,66 @@
-#' labelingCostQualityTradeoff
+#' Labeling cost/quality tradeoff
 #' 
+#' Initial Suggested Functions for simulating the tradeoff between labeling
+#' accuracy (P-probability that a label will correctly classify an instance) 
+#' and C (cost – price paid for the instance):
+#' 
+#' @param method Tradeoff function:
+#' 'Fix' returns the same probability for any price. (Mason and Watts, 2009). 
+#' 'Concave' produces lower probabilities in the cost range, where the highest
+#' probability is somewhere in the middle. (Donghui et al., 2009)
+#' 'Asymptotic' the probability increases.
+#' 'Quality' the strict probability to assign return, 
+#' e.g. costPerTask=0.8 would return probability=0.8.
+#' @param costPerTask Price paid to classify an instance.
+#' Need to be in the range [0.02,0.25].
+#' @return The probability \code{p}.
 #' 
 
-labelingCostQualityTradeoff <- function(method=c('Fix','Concave','Asymptotic'),
-                                        costPerLabel=NULL,
-                                        fixProbability=NULL) {
-    #'Returns the probability for a correct label given payment per label and tradeoff function type ('Fix','Concave','Asymptotic')
-    #'
+labelingCostQualityTradeoff <- function(method=c('Fix','Concave','Asymptotic','Quality'),
+                                        costPerTask=NULL,
+                                        fixProbability=NULL){
+    method  = tolower(method)
+    C_Range = c(0.02,0.25) # define the cost range
     # Check for null values. If TRUE then set the default
-    if (is.null(method))                 method <- 'Fix'
-    if (is.null(costPerLabel))       costPerLabel <- 0.1
+    if (is.null(method))                 method <- 'fix'
+    if (is.null(costPerTask))       costPerTask <- 0.1
     if (is.null(fixProbability)) fixProbability <- 0.75        
     # Check arguments validity
-    ## Cost
-    C = costPerLabel
-    C_Range = c(0.02,0.25)
-    if (!is.numeric(C) || C<C_Range[1] || C>C_Range[2]) {
-        stop('Unvalid cost per label')
+    ## costPerTask argument
+    if (method=="quality") {
+        if ((max(costPerTask)>1) || (min(costPerTask)<0))
+            stop('Unvalid costPerTask for method=Quality. Only numbers in the range [0,1] are valid')
     }
-    ## Probability
-    if (!is.numeric(fixProbability) || fixProbability<=0 || fixProbability>1){
-        stop('Probability sholud be in the range (0,1]')
-    }
+    else if (!is.numeric(costPerTask) || costPerTask<C_Range[1] || costPerTask>C_Range[2])
+        stop(paste0('Unvalid costPerTask. Only numbers in the range [',C_Range[1],',',C_Range[1],'] are valid'))
+    ## fixProbability argument
+    if (!is.numeric(fixProbability) || fixProbability<=0 || fixProbability>1)
+        stop('fixProbability sholud be in the range (0,1]')
     
-    if (method[1]=='Fix') {
-        p = rep(fixProbability,length(costPerLabel))               
-    } else if (method[1]=='Concave') {
+    
+    if (method=='fix') {
+        p = rep(fixProbability,length(costPerTask))               
+    } else if (method=='concave') {
+        C = costPerTask
         #                 p =-0.002*(C*100)^2+0.066*(C*100)+0.37
         p = 0.48+0.066*(100*C)-0.0022*(100*C)^2
+        #                 curve(0.48 +0.066*(100*x) + -0.0022*(100*x)^2, from=0.02, to=0.25)
+        ## Supply 3 points
+        #                 x = c(0.02,0.16,0.25) # Cost
+        #                 y = c(0.60,0.95,0.70) # Probability
+        ## Fin the coefficients for the polynom
+        #                 coeff <- lm(y~poly(x,degree=2,raw=TRUE))$coefficients
+        #                 p <- coeff[1] + coeff[1]*C + coeff[2]*C^2
         ## Make sure p \in [0,1], one p at a time
         p <- sapply(p,function(p) max(0,min(p,1)))
         #                 curve(coeff[1] + coeff[1]*x + coeff[2]*x^2, from=0.02, to=0.25)
-        
-    } else if (method[1]=='Asymptotic') {
+    } else if (method=='asymptotic') {
+        C = costPerTask
         p=1-1/(C*100) 
+    } else if (method=='quality') {
+        p=costPerTask
     } else {
         stop('Unknown tradeoff method')
     }    
     return(p)   
-}
+} # end function labelingCostQualityTradeoff
