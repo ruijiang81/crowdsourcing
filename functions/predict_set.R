f25789e23b00fa48e89aac4d0fcbbfa5d09cc423
Harel Lustiger <harel@loostiger.com>
2015-10-22 16:35:38 +0300

    minor changes to predict_set()

--- functions/predict_set.R
+++ functions/predict_set.R
@@ -36,7 +36,7 @@ predict_set = function(train_set,
                 ## Learn J4.8 tree with reduced error pruning (-R) and 
                 ## minimum number of instances set to 5 (-M 5):
                 #Weka_control = RWeka::Weka_control(R=TRUE, M=5)
-                Weka_control = Weka_control()
+                Weka_control = RWeka::Weka_control()
                 model = RWeka::J48(y ~ ., train_set, control=Weka_control)
                 y_hat = predict(model, test_set, type="probability")
                 predictions = as.vector(y_hat[,2])
@@ -47,7 +47,7 @@ predict_set = function(train_set,
             
             pred     = ROCR::prediction(predictions,test_set$y)
             perf_AUC = ROCR::performance(pred,"auc") #Calculate the AUC value
-            return(perf_AUC@y.values[[1]])
+            perf_AUC@y.values[[1]]
         },
         error = function(cond){ # if the model fitting or the evaluation failed, return AUC = 0.5 with a warning
             warning("predict_set could not fit/evalute a model, return AUC=NA")
