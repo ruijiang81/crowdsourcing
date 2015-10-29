deaa2106a2327c36c18d9215697e01812918929b
Harel Lustiger <harel@loostiger.com>
2015-10-29 15:53:16 +0200

    plot AUC as function of number of observations (dirty implementation)

--- scripts/analyse reports.R
+++ scripts/analyse reports.R
@@ -15,3 +15,21 @@ reports = import.reports(reports_folder)
 ## Calculate AUC(Cost)
 outputs = interpolate.reports(reports_folder, na.rm=FALSE)
 
+
+## Visualisations
+
+### Plot AUC as function of number of observations
+ggplot(outputs, aes(x=cost_intervals, y=average_holdout_cost_performance, group=payment_selection_criteria)) +
+    # Add lines to the plot
+    geom_line(aes(colour = payment_selection_criteria), size=1) +
+    # Add scatter points to the plot
+    geom_point(aes(colour = payment_selection_criteria), size=4) +
+    # Change axis labels
+    xlab("Cost of Model [$]") + ylab("AUC") + 
+    # Set axis limits and ticks
+    scale_x_continuous(breaks = seq(40,150,10), limits = c(40, 150)) +
+    #scale_y_continuous(limits = c(NA, 1)) +
+    # Theme settings
+    theme_bw() + theme(strip.text.x = element_blank(),
+                       strip.background = element_rect(colour="white", fill="white"),
+                       legend.position=c(.1,.9))
