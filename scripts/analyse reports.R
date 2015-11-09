522f72c05f389fbef0c065ebc245027d17ad6224
Harel Lustiger <harel@loostiger.com>
2015-11-09 12:59:50 +0200

    added to analysis: transition matrix

--- scripts/analyse reports.R
+++ scripts/analyse reports.R
@@ -25,33 +25,12 @@ reports = import.reports(reports_folder,
                          # Remove the "random" rule metadata
                          random.rm=FALSE)
 
-
-################################################################################
-## Discrete Markov Chains
-################################################################################
 rules = unique(metadata$payment_selection_criteria)
-sequence = subset(metadata, 
-                  repetition==1 & payment_selection_criteria %in% rules[1],
-                  select=pay)
-states = paste0(t(sequence),'$')
-library(markovchain)
-mcFit <- markovchainFit(data=states)
-## transition matrix
-tm = mcFit$estimate@transitionMatrix
-#colnames(tm) = rownames(tm) = paste0(colnames(tm),"$")
-cat("The probability from row payment into column payment")
-round(tm,2)
-##
-dtmcA <- new("markovchain",transitionMatrix=tm,
-             #states=c(),
-             name="Payment transition matrix") #create the DTMC
-plot(dtmcA)
 
 
 ################################################################################
 ## Distribution of payments per repetition (Batch-Wise)
 ################################################################################
-#round(prop.table(table(states)),2)
 library(manipulate)
 library(pander)
 
@@ -81,7 +60,6 @@ manipulate(
 ################################################################################
 ## Distribution of payments per repetition (Repetition-Wise)
 ################################################################################
-#round(prop.table(table(states)),2)
 library(manipulate)
 library(pander)
 
@@ -108,75 +86,80 @@ manipulate(
 )# end payment distribution manipulate
 
 
+################################################################################
+## Discrete Markov Chains
+################################################################################
+library(markovchain)
+manipulate(
+    {
+        sub_dataset = metadata[metadata$payment_selection_criteria %in% rule,]
+        ## Create transition matrices
+        tm = list()
+        for(r in 1:max(sub_dataset$repetition))
+        {
+            sequence = subset(sub_dataset, 
+                              repetition==r,
+                              select=pay)  
+            sequence = paste0(t(sequence),'$')
+            tm[[r]] = markovchainFit(data=sequence)$estimate@transitionMatrix
+        } #end 
+        
+        
+        ## Select transition matrix
+        if(f_rep){ ## Aggregate all repetitions
+            transitionMatrix = tm[[1]]
+            for(r in 2:max(sub_dataset$repetition)) transitionMatrix = transitionMatrix + tm[[r]]
+            transitionMatrix=transitionMatrix/r
+            
+        } else {
+            transitionMatrix = tm[[n_rep]]
+        }
+        
+        
+        ## Plot chain
+        dtmcA <- new("markovchain",transitionMatrix=transitionMatrix,
+                     #states=c(),
+                     name="Payment transition matrix") #create the DTMC
+        set.seed(2015)
+        plot(dtmcA)
+        
+        ## Print Chain
+        cat("\nThe probability from row payment into column payment\n")
+        round(transitionMatrix,2)
+    },
+    n_rep = slider(1, max(metadata$repetition), 1, "Choose Repetition", 1),
+    rule = picker("delta_auc_div_total_cost","max_quality","max_ratio","max_total_ratio"),
+    f_rep = checkbox(FALSE, "All repetition")
+) #end HMM manipulation
 
 
-
-
-# manipulate(
-#     {
-#         rules = c("delta_auc_div_total_cost","max_quality","max_ratio","max_total_ratio")
-#         flags = c(f1,f2,f3,f4)
-#         sequence = c()
-#         for(flag in flags){
-#             if(flag){
-#                 sequence = rbind(sequence,subset(metadata,
-#                                                  payment_selection_criteria %in% rules[flag],
-#                                                  select=pay)) 
-#             } # end if
-#         } # end for
-#         
-#         states = paste0(t(sequence),'$')
-#         library(markovchain)
-#         mcFit <- markovchainFit(data=states)
-#         ## transition matrix
-#         tm = mcFit$estimate@transitionMatrix
-#         cat("\n","The probability from row payment into column payment")
-#         # performance.table <- table(some_table)
-#         pandoc.table(round(tm,2), style = "grid", justify = 'left', caption = 'Performance table')
-#         
-#         ##
-#         dtmcA <- new("markovchain",transitionMatrix=tm,
-#                      #states=c(),
-#                      name="Payment transition matrix") #create the DTMC
-#         plot(dtmcA)
-#     },
-#     f1 = checkbox(F, rules[1]), 
-#     f2 = checkbox(F, rules[2]),
-#     f3 = checkbox(T, rules[3]),
-#     f4 = checkbox(F, rules[4])
-# ) # end manipulate
-
-
-
-
-
-# ## Calculate AUC(Cost)
-# outputs = interpolate.reports(reports_folder, na.rm=FALSE)
-# 
-# 
-# ## Visualisations
-# ### Create plots dir
-# plot_dir  = file.path(getwd(),"plots")
-# dir.create(plot_dir,showWarnings=F)
-# ### Plot AUC as function of number of observations
-# ggplot(outputs, aes(x=cost_intervals, y=average_holdout_cost_performance, group=payment_selection_criteria)) +
-#     # Add lines to the plot
-#     geom_line(aes(colour = payment_selection_criteria), size=1) +
-#     # Add scatter points to the plot
-#     geom_point(aes(colour = payment_selection_criteria), size=4) +
-#     # Change axis labels
-#     xlab("Cost of Model [$]") + ylab("AUC") + 
-#     # Set axis limits and ticks
-#     scale_x_continuous(breaks = seq(40,150,10), limits = c(40, 150)) +
-#     #scale_y_continuous(limits = c(NA, 1)) +
-#     # Theme settings
-#     theme_bw() + theme(strip.text.x = element_blank(),
-#                        strip.background = element_rect(colour="white", fill="white"),
-#                        legend.position=c(.1,.9))
-# ### Export "Auc vs. Cost" plot
-# plot_name = paste0('(',unique(outputs$DATABASE_NAME),')',
-#                    '(',unique(outputs$model_inducer),')',
-#                    '(',unique(outputs$cost_function_type),')',
-#                    '(','Auc vs. Cost',')',
-#                    ".png")
-# ggsave(file=file.path(plot_dir,plot_name), width=12, height=8)
+################################################################################
+## Visualisation
+################################################################################
+## Calculate AUC(Cost)
+outputs = interpolate.reports(reports_folder, na.rm=FALSE)
+### Create plots dir
+plot_dir  = file.path(getwd(),"plots")
+dir.create(plot_dir,showWarnings=F)
+### Plot AUC as function of number of observations
+ggplot(outputs, aes(x=cost_intervals, y=average_holdout_cost_performance, group=payment_selection_criteria)) +
+    # Add lines to the plot
+    geom_line(aes(colour = payment_selection_criteria), size=1) +
+    # Add scatter points to the plot
+    geom_point(aes(colour = payment_selection_criteria), size=4) +
+    # Change axis labels
+    xlab("Cost of Model [$]") + ylab("AUC") + 
+    # Set axis limits and ticks
+    scale_x_continuous(breaks = seq(40,150,10), limits = c(40, 150)) +
+    #scale_y_continuous(limits = c(NA, 1)) +
+    # Theme settings
+    theme_bw() + theme(strip.text.x = element_blank(),
+                       strip.background = element_rect(colour="white", fill="white"),
+                       legend.position=c(.1,.9))
+### Export "Auc vs. Cost" plot
+plot_name = paste0('(',unique(outputs$DATABASE_NAME),')',
+                   '(',unique(outputs$model_inducer),')',
+                   '(',unique(outputs$cost_function_type),')',
+                   '(','Auc vs. Cost',')',
+                   ".png")
+ggsave(file=file.path(plot_dir,plot_name), width=12, height=8)
