1e5eece45a5d011dbb1af5245ec6caa137ca8d0b
Harel Lustiger <harel@loostiger.com>
2015-12-09 09:43:08 +0200

    use the 'tm' package to create movies review dataset

--- data/Movie Review/dpp.R
+++ data/Movie Review/dpp.R
@@ -1,94 +1,97 @@
 ## Initialization
-### Detects the number of cores and prepares for parallel run
-# cl <- makeCluster(detectCores()-cores_not_to_use,outfile="")   
-# registerDoParallel(cl)
-
-## Get the data
+cat("\014"); rm(list = ls())
 #install.packages("data.table")
+#install.packages("tm")
+#install.packages("FSelector")
+
+################################################################################
+## Get the data
+################################################################################
 library(data.table)
-dataset_neg <- fread("./data/Movie Review/rt-polarity.neg", sep="\n", data.table=FALSE, col.names="review")
-dataset_pos <- fread("./data/Movie Review/rt-polarity.pos", sep="\n", data.table=FALSE, col.names="review")
-dataset <- rbind(dataset_neg, dataset_pos)
-labels <- as.factor(c(rep("N",nrow(dataset_neg)), rep("P",nrow(dataset_pos))))
+reviews_neg <- fread("./data/Movie Review/rt-polarity.neg", sep="\n", data.table=FALSE, col.names="review", stringsAsFactors=FALSE)
+reviews_pos <- fread("./data/Movie Review/rt-polarity.pos", sep="\n", data.table=FALSE, col.names="review", stringsAsFactors=FALSE)
+reviews <- rbind(reviews_neg, reviews_pos)
+labels  <- as.factor(c(rep("N",nrow(reviews_neg)), rep("P",nrow(reviews_pos))))
 
 
-## Bag of Words
-# install.packages("qdap")
-# library("qdap")
-# words <- all_words(dataset, apostrophe.remove=TRUE)
-# head(words)
-# bag_o_words(dataset, apostrophe.remove=TRUE)
+################################################################################
+## Text mining
+################################################################################
+library(tm)
+## Set up a source for the text
+reviews_source <- VectorSource(reviews[,])
+
+## Create a corpus from that source 
+corpus <- Corpus(reviews_source)
+
+## Cleaning the text
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, removeWords, stopwords("english")) # or use stopwords("SMART")
+corpus <- tm_map(corpus, stemDocument) # requires the "SnowballC" package
+#getTransformations() # display what the data cleaning options are
+#stopwords("english") # what is getting removed
+
+## Create the document-term matrix.
+dtm <- DocumentTermMatrix(corpus)
+dim(dtm)
+dtm2 <- as.matrix(dtm)
+
+## Create logical matrix
+dtm_l    <- dtm2>0 #convert counts matrix to logical matrix
+labels_l <- labels=="P"
 
 
-## Create a term-document matrix from a corpus
-#install.packages(c("tm","bigmemory"))
-library("tm")
-library("bigmemory")
-corpus <- Corpus(VectorSource(dataset[1:10660,]))
-DTM <- DocumentTermMatrix(corpus,
-                          control = list())
-DTM
 
-M=as.big.matrix(x=as.matrix(DTM))#convert the DTM into a bigmemory object using the bigmemory package 
-M=as.matrix(M)#convert the bigmemory object again to a regular matrix
-M_l = M>0 #convert counts matrix to logical matrix
-dim(M)
-
-
-## Remove near zero variance predictors
-# library("caret")
-# p = 100
-# freq = t(round(prop.table(apply(M_l, 2, table),2),4))[,2]
-# freq = sort(freq, decreasing=TRUE)
-# hist(freq[1:100],20)
-# index_cols = colnames(M_l) %in% names((freq[1:p]))
-# M_nzr = nearZeroVar(M_l[,index_cols], freqCut = 95/5, uniqueCut = 10, saveMetrics = TRUE, 
-#                     foreach = FALSE, allowParallel = TRUE)
-# M_nzr
-#' freqRatio	    the ratio of frequencies for the most common value over the second most common value
-#' percentUnique	the percentage of unique data points out of the total number of data points
-#' zeroVar	        a vector of logicals for whether the predictor has only one distinct value
-#' nzv	            a vector of logicals for whether the predictor is a near zero variance predictor
-#' #
-# checkConditionalX(x=as.data.frame(M[,1:10]), y=labels)
-#
-# https://rpubs.com/nishantsbi/92510
-# sparse = tm::removeSparseTerms(frequencies, 0.995)
-
-
-## Information gain
-# install.packages("FSelector")
-# library("FSelector")
-# D = as.data.frame(cbind(M_l[,index_col],labels))
-# IG = information.gain(labels~., D)
-# IG
-
+################################################################################
+## Word Selection
+################################################################################
+dtm_l_df = as.data.frame(cbind(dtm_l, "labels"=labels_l))
 set.seed(2016)
-index_train = sample(1:nrow(dataset),round(0.7*nrow(dataset)))
+index_train = sample(1:nrow(reviews),round(0.7*nrow(reviews)))
 p = 40
 
+## Correlation
 # Find the correlation between the independent variables and the dependent variable
-C = cor(M_l,labels=="P")
+W = cor(dtm_l, labels_l)
 # Pick the top 'm' correlated independent variables
-names(C) = colnames(M_l)
-sort(abs(C), decreasing = TRUE)[1:p]
-index_col = colnames(M_l)[colnames(M_l) %in% names(sort(abs(C), decreasing = TRUE)[1:p])]
+names(W) = colnames(dtm_l)
+W = sort(abs(W), decreasing = TRUE)
+index_col = c(colnames(dtm_l)[colnames(dtm_l) %in% names(W)[1:p]],"labels")
 
-D     = data.frame(M_l[,index_col],labels=(labels=="P"))
-mdl   = glm(labels~., D[index_train, ], family = "binomial")
+
+## Informaion Gain
+library("FSelector")
+start.time <- Sys.time()
+#for(chunk=)
+IG = information.gain(labels~., dtm_l_df)
+Sys.time() - start.time
+
+
+
+################################################################################
+## Fitting model on the data
+################################################################################
+library(caret)
+library(ROCR)
+mdl = glm(labels~., dtm_l_df[index_train,index_col], family = "binomial")
+summary(mdl)
+
+
 #y_hat = predict(mdl, D[-index_train, ])
 #caret::confusionMatrix(y_hat>0,D[-index_train,"labels"])
-y_hat    = predict(mdl, D[-index_train,], type="response")
-pred     = ROCR::prediction(as.vector(y_hat),D[-index_train,"labels"])
+y_hat    = predict(mdl, dtm_l_df[-index_train,index_col], type="response")
+pred     = ROCR::prediction(as.vector(y_hat),dtm_l_df[-index_train,"labels"])
 perf_AUC = ROCR::performance(pred,"auc") #Calculate the AUC value
 perf_AUC@y.values[[1]]
 
 
-# p=40;  0.68/0.68
+
+# p=40;  0.68
 # p=100; 0.75
 # p=400; 0.83
+ 
 
-#summary(mdl)
-
-# stopCluster(cl) 
-write.csv(ifelse(D==FALSE,0,1), "MR40_dataset.csv", row.names=F)
+# write.csv(ifelse(dtm_l_df[,index_col]==FALSE,0,1), "MR40_dataset.csv", row.names=F)
