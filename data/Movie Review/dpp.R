da653cec7531e647f36079c44199a76a9fb27778
Harel Lustiger <harel@loostiger.com>
2015-12-28 12:54:14 +0200

    Movies Reviews with 300 features

--- data/Movie Review/dpp.R
+++ data/Movie Review/dpp.R
@@ -1,12 +1,17 @@
-## Initialization
+##################
+# Initialization #
+##################
 cat("\014"); rm(list = ls())
-#install.packages("data.table")
-#install.packages("tm")
-#install.packages("FSelector")
+# install.packages("data.table")
+# install.packages("tm")
+# install.packages("FSelector")
+# install.packages("caret")
+# install.packages("ROCR")
 
-################################################################################
-## Get the data
-################################################################################
+
+################
+# Get the data #
+################
 library(data.table)
 reviews_neg <- fread("./data/Movie Review/rt-polarity.neg", sep="\n", data.table=FALSE, col.names="review", stringsAsFactors=FALSE)
 reviews_pos <- fread("./data/Movie Review/rt-polarity.pos", sep="\n", data.table=FALSE, col.names="review", stringsAsFactors=FALSE)
@@ -14,9 +19,9 @@ reviews <- rbind(reviews_neg, reviews_pos)
 labels  <- as.factor(c(rep("N",nrow(reviews_neg)), rep("P",nrow(reviews_pos))))
 
 
-################################################################################
-## Text mining
-################################################################################
+###############
+# Text mining #
+###############
 library(tm)
 ## Set up a source for the text
 reviews_source <- VectorSource(reviews[,])
@@ -45,35 +50,35 @@ labels_l <- labels=="P"
 
 
 
-################################################################################
-## Word Selection
-################################################################################
+##################
+# Word Selection #
+##################
 dtm_l_df = as.data.frame(cbind(dtm_l, "labels"=labels_l))
 set.seed(2016)
 index_train = sample(1:nrow(reviews),round(0.7*nrow(reviews)))
-p = 40
+p = 300
 
-## Correlation
-# Find the correlation between the independent variables and the dependent variable
+# Correlation
+## Find the correlation between the independent variables and the dependent variable
 W = cor(dtm_l, labels_l)
-# Pick the top 'm' correlated independent variables
+## Pick the top 'm' correlated independent variables
 names(W) = colnames(dtm_l)
 W = sort(abs(W), decreasing = TRUE)
 index_col = c(colnames(dtm_l)[colnames(dtm_l) %in% names(W)[1:p]],"labels")
 
 
-## Informaion Gain
-library("FSelector")
-start.time <- Sys.time()
-#for(chunk=)
-IG = information.gain(labels~., dtm_l_df)
-Sys.time() - start.time
+# Informaion Gain
+# library("FSelector")
+# start.time <- Sys.time()
+# #for(chunk=)
+# IG = information.gain(labels~., dtm_l_df)
+# Sys.time() - start.time
 
 
 
-################################################################################
-## Fitting model on the data
-################################################################################
+#############################
+# Fitting model on the data #
+#############################
 library(caret)
 library(ROCR)
 mdl = glm(labels~., dtm_l_df[index_train,index_col], family = "binomial")
@@ -86,12 +91,23 @@ y_hat    = predict(mdl, dtm_l_df[-index_train,index_col], type="response")
 pred     = ROCR::prediction(as.vector(y_hat),dtm_l_df[-index_train,"labels"])
 perf_AUC = ROCR::performance(pred,"auc") #Calculate the AUC value
 perf_AUC@y.values[[1]]
-
-
-
 # p=40;  0.68
 # p=100; 0.75
 # p=400; 0.83
- 
 
-# write.csv(ifelse(dtm_l_df[,index_col]==FALSE,0,1), "MR40_dataset.csv", row.names=F)
+
+#######################
+# Shuffle the dataset #
+#######################
+set.seed(2016)
+dataset = dtm_l_df
+dataset = dataset[sample(nrow(dataset)),]
+
+
+#######################
+# Store the dataset #
+#######################
+file_name = paste0("dataset_v",p,".csv")
+file_path = file.path(".","data","Movie Review",file_name)
+file_name
+write.csv(ifelse(dataset[,index_col]==FALSE,0,1), file_path, row.names=F)
--- data/Movie Review/import dataset.R
+++ data/Movie Review/import dataset.R
@@ -1,1 +1,1 @@
-dataset = read.csv("./data/Movie Review/dataset_v40.csv", colClasses = "factor")
+dataset = read.csv("./data/Movie Review/dataset_v300.csv", colClasses = "factor")
