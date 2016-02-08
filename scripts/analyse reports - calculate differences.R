10cc8024673e6be26ea28cb910b440d89fb301e8
Harel Lustiger <harel@loostiger.com>
2016-02-08 12:50:15 +0200

    query all function for the corresponding costs where the random gets max AUC

--- scripts/analyse reports - calculate differences.R
+++ scripts/analyse reports - calculate differences.R
@@ -20,7 +20,7 @@ head(outputs)
 
 
 ##########################################
-# Export "AUC as function of Cost" tabel #
+# Export "AUC as function of Cost" table #
 ##########################################
 lower_bound = 50
 upper_bound = 150
@@ -42,13 +42,32 @@ head(AUC.tabel)
 ##########################################
 # Export "Cost as function of AUC" table #
 ##########################################
-indicator_of_the_random_key = tolower(substr(outputs$payment_selection_criteria,1,6)) %in% "random"
+key_dic = unique(outputs[,c("key","payment_selection_criteria")])
+random_value  = key_dic[tolower(substr(key_dic$payment_selection_criteria,1,6)) %in% "random","payment_selection_criteria"]
+random_key    = key_dic[tolower(substr(key_dic$payment_selection_criteria,1,6)) %in% "random","key"]
+random_output = subset(outputs, key==random_key)
 
-randomRule_range = range(outputs[indicator_of_the_random_key,"average_holdout_cost_performance"], na.rm=T)
-query_points = seq(randomRule_range[1], randomRule_range[2], length.out=100)
+
+# Find the AUC(Cost=50) and max(AUC) of the random rule, create 100 linear 
+# spaced values among them, and query the other methods what is the price to get 
+# the same AUC
+random_AUC_at_50 = subset(random_output, cost_intervals==50, select=average_holdout_cost_performance) 
+random_AUC_max   = max(random_output[,"average_holdout_cost_performance"], na.rm=T)
+query_points = seq(random_AUC_at_50[[1]], random_AUC_max, length.out=100)
 
 Cost.tabel = Cost.as.a.function.of.AUC(outputs, query_points)
+Cost.tabel
 
+
+# Find the max(AUC) of the random rule, and query the other methods what is the 
+# price to get the same AUC
+random_tuple  = random_output[which.max(random_output[,"average_holdout_cost_performance"]),c("cost_intervals","average_holdout_cost_performance")]
+
+Cost.tabel = Cost.as.a.function.of.AUC(outputs, query_points=random_tuple[,"average_holdout_cost_performance"])
+Cost.tabel[,random_value] = random_tuple[,"cost_intervals"] # append random real cost value
+Cost.tabel
+
+# Export results to csv file
 report_dir = file.path(getwd(),"results")
 file_name  = paste0('(','Cost as a function of AUC',')',
                     '(','Intervales of size ',interval_size,')',
