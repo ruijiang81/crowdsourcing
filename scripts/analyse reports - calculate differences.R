277a411faf61c272dbe6b724dcee5efd4d93fb1f
Harel Lustiger <harel@loostiger.com>
2016-04-06 10:07:13 +0300

    Cost as a function of AUC features comparison at the AUC point (within random rule) corresponds to 150$

--- scripts/analyse reports - calculate differences.R
+++ scripts/analyse reports - calculate differences.R
@@ -61,21 +61,26 @@ for(k in 1:nrow(report_param))
 ##########################################
 for(k in 1:nrow(report_param))
 {
+    lower_bound = 50
+    upper_bound = 150
+    
     # Subset the output
     cases = !logical(nrow(outputs))
     for(p in 1:length(report_div)) 
         cases = (cases & outputs[,report_div[p]] %in% report_param[k,p])
     output = outputs[cases,]
+    output = subset(output, 
+                    lower_bound<=cost_intervals & cost_intervals<=upper_bound)
     
     key_dic = unique(output[,c("key","payment_selection_criteria")])
     random_value  = key_dic[tolower(substr(key_dic$payment_selection_criteria,1,6)) %in% "random","payment_selection_criteria"]
     random_key    = key_dic[tolower(substr(key_dic$payment_selection_criteria,1,6)) %in% "random","key"]
-    random_output = subset(outputs, key==random_key)
+    random_output = subset(output, key==random_key)
     
     
     # Find the AUC(Cost=50) and max(AUC) of the random rule, create 100 linear 
-    # spaced values among them, and query the other methods what is the price to get 
-    # the same AUC
+    # spaced values among them, and query the other methods what is the price to 
+    # get the same AUC
     random_AUC_at_50 = subset(random_output, cost_intervals==50, select=average_holdout_cost_performance) 
     random_AUC_max   = max(random_output[,"average_holdout_cost_performance"], na.rm=T)
     query_points     = seq(random_AUC_at_50[[1]], random_AUC_max, length.out=100)
@@ -84,11 +89,20 @@ for(k in 1:nrow(report_param))
     Cost.tabel
     
     
-    # Find the max(AUC) of the random rule, and query the other methods what is the 
-    # price to get the same AUC
-    random_tuple  = random_output[which.max(random_output[,"average_holdout_cost_performance"]),c("cost_intervals","average_holdout_cost_performance")]
+    # Find the index for a reference AUC value within random. 
+    # there are 2 options:
+    # (1) Find the max(AUC) of the random rule, and query the other methods  
+    #     what is the price to get the same AUC
+    # (2) Take the AUC of the random rule with the max cost (typically 150$)
     
-    Cost.tabel = Cost.as.a.function.of.AUC(output, query_points=random_tuple[,"average_holdout_cost_performance"])
+    # Option (1)
+    comparison_cost_point_index = which.max(random_output[,"average_holdout_cost_performance"])
+    # Option (2)
+    # comparison_cost_point_index = which.max(random_output[,"cost_intervals"])
+    
+    
+    random_tuple = random_output[comparison_cost_point_index,c("cost_intervals","average_holdout_cost_performance")]
+    Cost.tabel   = Cost.as.a.function.of.AUC(output, query_points=random_tuple[,"average_holdout_cost_performance"])
     Cost.tabel[,random_value] = random_tuple[,"cost_intervals"] # append random real cost value
     Cost.tabel
     
