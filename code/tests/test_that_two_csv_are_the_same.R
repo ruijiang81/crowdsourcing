edb155c04055c13a3a327daf899709fa29833c56
harell <harel@loostiger.com>
2018-06-16 16:05:52 +1200

    more informative test

--- code/tests/test_that_two_csv_are_the_same.R
+++ code/tests/test_that_two_csv_are_the_same.R
@@ -27,9 +27,9 @@ test_that_two_csv_files_are_identical <- function(file_path_1, file_path_2){
     ####################
     file_temp <- data.frame()
     for(r in unique(file_2$repetition)){
-        S <- file_2 %>% filter(repetition == r) %>% 
+        S <- file_2 %>% filter(repetition == r) %>%
             summarise(batch = max(batch), repetition = max(repetition))
-        file_temp <- 
+        file_temp <-
             bind_rows(
                 file_temp,
                 file_1 %>% filter(repetition == S$repetition, batch <= S$batch)
@@ -37,6 +37,7 @@ test_that_two_csv_files_are_identical <- function(file_path_1, file_path_2){
     }
     file_1 <- file_temp
     assertive::assert_is_non_empty(file_1)
+    assertive::assert_have_same_dims(file_1, file_2, severity = "stop")
     #'
     #########
     # Tests #
@@ -44,26 +45,36 @@ test_that_two_csv_files_are_identical <- function(file_path_1, file_path_2){
     #' test that columns are identical
     assert_set_equal(colnames(file_1), colnames(file_2), add = collection)
     reportAssertions(collection)
+    #'
     #' test that columns content is identical  
     for(rep in unique(file_1$repetition)){
+        
         cat_40("Testing Repetition" %+% " " %+% rep)
+        R1 <- file_1 %>% filter(repetition == rep)
+        R2 <- file_2 %>% filter(repetition == rep)
+        # cat("\n# " %+% nrow(R1) %+% " vs. " %+% nrow(R2) %+%" rows")
+        cat("\n")
+        assertive::assert_have_same_dims(file_1, file_2, severity = "message")
         for(col_name in colnames(file_1)){
-            cat("\n*\t", col_name)
+            cat("\n# ->", col_name, "")
             if(is.numeric(file_1[,col_name])){
                 file_1[,col_name] %<>% round(5)
                 file_2[,col_name] %<>% round(5)
             }
             # Subset data
-            E1 <- file_1 %>% filter(repetition == rep) %>% .[[col_name]]
-            E2 <- file_2 %>% filter(repetition == rep) %>% .[[col_name]]
-            # Check equal size
-            assertive::assert_are_same_length(E1, E2, severity = "message")
-            assertive::assert_are_same_length(E1, E2, severity = "warning")
+            E1 <- R1 %>% .[[col_name]]
+            E2 <- R2 %>% .[[col_name]]
+            # Quantify errors
+            if(is.numeric(file_1[,col_name]) & length(E1) == length(E2)){
+                square_error = (E1-E2)^2
+                cat("\t| SSE =", sum(square_error, rm.na = TRUE))
+                cat("\t| MSE =", mean(square_error, rm.na = TRUE))
+                cat('\t| ')
+                file_2[,col_name] %<>% round(5)
+            }
             # Check equal values
             assertive::assert_are_set_equal(E1, E2, severity = "message")
             assertive::assert_are_set_equal(E1, E2, severity = "warning")
-            # Show problems
-            # reportAssertions(collection)
         }# end for col_names
     }# end for repetitions
     #'
@@ -79,7 +90,6 @@ test_that_two_csv_files_are_identical <- function(file_path_1, file_path_2){
 # ---------------------------------------------------------------------------- #
 file_name_1 <- "A"
 file_name_2 <- "B"
-
 
 file_path_1 <- file.path(getwd(), "results", paste0(file_name_1, ".csv"))
 file_path_2 <- file.path(getwd(), "results", paste0(file_name_2, ".csv"))
