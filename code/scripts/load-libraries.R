ce9de29148516d1711487f621f4d2881bbbb35ce
harell <harel@loostiger.com>
2018-07-01 13:43:01 +1200

    metadata analysis

--- code/scripts/load-libraries.R
+++ code/scripts/load-libraries.R
@@ -65,7 +65,7 @@ libraries_on_CRAN <- c(
     # <http://r-pkgs.had.co.nz/tests.html>
     "testthat",
     # Data Manipulation
-    "tidyverse", "data.table", "dplyr", "plyr", "magrittr",
+    "tidyverse", "data.table", "dplyr", "magrittr",
     # Dynamic Report Generation in R
     "knitr", "pander",  
     # Visualization tools
