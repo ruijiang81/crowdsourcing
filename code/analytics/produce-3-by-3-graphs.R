6d8afed63ba954da116d5e32dcd12c8b70781738
harell <harel@loostiger.com>
2018-09-10 16:06:04 +1200

    ggplot 3rd iteration

--- code/analytics/produce-3-by-3-graphs.R
+++ code/analytics/produce-3-by-3-graphs.R
@@ -1,25 +1,34 @@
 ################################################################################
 ## Analyze Reports - Produce Graphs
 ################################################################################
-## Initialization
 source(file.path(getwd(), "code", "scripts", "setup.R"))
 cat_80("Metadata Pro Processing")
+plot_type <- c("Performance vs. cost",
+               "Avg label quality vs. cost",
+               "No. of labels acquired vs. cost")[1]
 #'
-################################################################################
-## Preprocess the data (under ../results/)
-################################################################################
-source(file.path(k_path_analytics, "processing-the-report-files.R"))
-source(file.path(k_path_analytics, "processing-the-metadata-files.R"))
-source(file.path(k_path_analytics, "processing-the-ledger-files.R"))
-#'
-################
-# Get the data #
-################
+##############################
+# Get the project's metadata #
+##############################
 policies_attributes <- read_csv(file.path(k_path_dictionaries, "policies-attributes.csv"))
-data_report <- read_csv(file.path(k_path_results, "processed", "reports-by-dollar.csv"))
-data_metadata <- read_csv(file.path(k_path_results, "processed", "metadata-by-dollar.csv"))
-data_ledger <- read_csv(file.path(k_path_results, "processed", "ledgers-by-dollar.csv"))
-data <- data_report
+analytics_attributes <- read_csv(file.path(k_path_dictionaries, "analytics-attributes.csv"))
+analytics_attributes <- analytics_attributes %>% filter(name == plot_type)
+#'
+##########################
+# Get the project's data #
+##########################
+if(plot_type == "Performance vs. cost"){
+    source(file.path(k_path_analytics, "processing-the-report-files.R"))
+    data <- read_csv(file.path(k_path_results, "processed", "reports-by-dollar.csv"))
+} else if(plot_type == "Performance vs. cost") {
+    source(file.path(k_path_analytics, "processing-the-metadata-files.R"))
+    data <- read_csv(file.path(k_path_results, "processed", "metadata-by-dollar.csv"))
+} else if(plot_type == "Performance vs. cost") {
+    source(file.path(k_path_analytics, "processing-the-ledger-files.R"))
+    data <- read_csv(file.path(k_path_results, "processed", "ledgers-by-dollar.csv"))
+} else {
+    stop("Plot type is unknown")
+}
 #'
 ###############################
 # Configure ggplot attributes #
@@ -30,6 +39,8 @@ mfcol <- 3
 ## What is the text and line sizes?
 text_size <- rel(0.8)
 line_size <- rel(0.8)
+## What is the x axis range?
+xlim <- c(40, 150)
 #'
 ################################################################################
 ## Visualization
@@ -43,7 +54,8 @@ for(k in seq_along(pairs)){
     ggplot_data <- 
         data %>% 
         filter(database_name %in% pairs[k, "database_name"],
-               cost_function_type %in% pairs[k, "cost_function_type"])
+               cost_function_type %in% pairs[k, "cost_function_type"],
+               cost_so_far <= max(xlim), cost_so_far >= min(xlim))
     # Aggregate the data
     ggplot_data <-
         ggplot_data %>%
@@ -80,18 +92,23 @@ for(k in seq_along(pairs)){
         geom_line(size = line_size) +
         scale_colour_manual(values = policies_colors,
                             labels = policies_alias) + 
+        # X and Y axes
+        # scale_x_continuous(limits = xlim) + 
         # Plot labels
-        labs(color = "") +
+        labs(x = analytics_attributes$xlab,
+             y = analytics_attributes$ylab,
+             color = "") +
         # Theme settings
         theme_bw() + 
         theme(
-            legend.background = element_rect(fill = "transparent")
+            legend.background = element_rect(fill = "transparent"),
+            aspect.ratio = 1
         )
-        
+    
     # Adjust the legend
     reposition_legend(aplot = fig, position = 'bottom right')
-        
-        
+    
+    
     # Theme layers
     # scale_colour_manual(name = "",
     #                     values = c(myline1="red", myline2="blue"))
